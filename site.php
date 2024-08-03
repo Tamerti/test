5cad3cf692cd7eac4fc4d6c42b865b95866c74d3
Eric <95740568+Tamerti@users.noreply.github.com>
2024-08-03 17:14:44 +0700

    Update site.php

--- site.php
+++ site.php
@@ -4,4 +4,4 @@ $str = substr($str, strpos($str, "players"));
 $str = substr($str, 0, strpos($str, ",maxplayers:"));
 $str = preg_replace('/[^0-9 ,]/', '', $str);
 if(!$str) $str = 0;
-echo '{"query":[{"number":1,"name":"Cladman","ip":"46.174.51.153","port":7777,"online":' .$str .',"maxplayers":1000,"password":false}]}';
+echo '{"query":[{"number":1,"name":"LSRP-VN","ip":"104.234.180.39","port":7777,"online":' .$str .',"maxplayers":150,"password":false}]}';
