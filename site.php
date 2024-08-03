e9535314f957fa205ca0734da176db9d8a6cb049
Eric <95740568+Tamerti@users.noreply.github.com>
2024-08-03 20:26:44 +0700

    Update site.php

--- site.php
+++ site.php
@@ -1,8 +1,8 @@
 <?php
 require_once("SampQueryAPI.php"); 
 
-$query = new SampQueryAPI('46.174.51.153', '7777'); 
+$query = new SampQueryAPI('104.234.180.39', '7777'); 
 $serverInfo = $query->getInfo(); 
 $serverRules = $query->getRules(); 
 
-echo('{"query":[{"number":1,"name":"Cladman","ip":"46.174.51.153","port":7777,"online":'. $serverInfo['players'] .',"maxplayers":300,"password":false}]}');
+echo('{"query":[{"number":3,"name":"[LS:RP] Los Santos Roleplay","ip":"104.234.180.39","port":7777,"online":'. $serverInfo['players'] .',"maxplayers":150,"password":false}]}');
