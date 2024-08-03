3dd0a0084caa3f29ba164c743608803ccf1979c8
Eric <95740568+Tamerti@users.noreply.github.com>
2024-08-03 19:03:22 +0700

    Update site.php

--- site.php
+++ site.php
@@ -1,7 +1,8 @@
 <?php
-$str = file_get_contents('https://samp-stats.ru/web/api-18794.js');
-$str = substr($str, strpos($str, "players"));
-$str = substr($str, 0, strpos($str, ",maxplayers:"));
-$str = preg_replace('/[^0-9 ,]/', '', $str);
-if(!$str) $str = 0;
-echo '{"query":[{"number":1,"name":"LSRP-VN","ip":"104.234.180.39","port":7777,"online":' .$str .',"maxplayers":150,"password":false}]}';
+require_once("SampQueryAPI.php"); 
+
+$query = new SampQueryAPI('46.174.51.153', '7777'); 
+$serverInfo = $query->getInfo(); 
+$serverRules = $query->getRules(); 
+
+echo('{"query":[{"number":1,"name":"Cladman","ip":"46.174.51.153","port":7777,"online":'. $serverInfo['players'] .',"maxplayers":300,"password":false}]}');
