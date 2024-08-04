d2cfe085b74f650a07ec2f78985542038feb2327
Eric <95740568+Tamerti@users.noreply.github.com>
2024-08-04 07:30:47 +0700

    Create samp.js

---
+++ samp.js
@@ -0,0 +1,28 @@
+
+	var api =
+	{	ip:'46.174.51.153',
+
+		port:'7777',
+
+		status:'0',
+
+		mapname:'-',
+
+		version:'',
+
+		weburl:'',
+
+		players:'0',
+
+		maxplayers:'0',
+
+		hostname:'Arizona RP | Cladman | 176.32.39.26:7777',
+
+		gamemode:'',
+
+		votes:'147',
+
+		plinfo:[
+		
+	]};
+	
